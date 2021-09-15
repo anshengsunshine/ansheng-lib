882c95b0ee378bf2acb15139c24368ba92f551c1
anshengsunshine <646143658@qq.com>
2021-09-16 01:54:13 +0800

    css配置修改

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from "rollup-plugin-babel"
 import commonjs from "rollup-plugin-commonjs"
 import postcss from 'rollup-plugin-postcss'
 import vue from "rollup-plugin-vue"
-import autoprefixer from "autoprefixer"
 import { terser } from "rollup-plugin-terser"
 
 export default {
@@ -25,13 +24,26 @@ export default {
             exclude: 'node_modules/**'  // 转换 es6 语法 
         }),
         vue({   // 编译 vue 代码，并为 vue 组件的样式加前缀
-            style: {
-                postcssPlugins: [
-                    autoprefixer()
-                ]
-            }
+            css: true,
+            compileTemplate: true
         }),
-        postcss(),
+        // postcss(),
+        // postcss({
+        //     // 把 css 插入到 style 中
+        //     // inject: true,
+        //     // 把 css 放到和js同一目录
+        //     extract: true
+        // }),
+        postcss([
+            require("postcss-modules")({
+                getJSON: function (cssFileName, json, outputFileName) {
+                    var path = require("path");
+                    var cssName = path.basename(cssFileName, ".css");
+                    var jsonFileName = path.resolve("./dist/" + cssName + ".json");
+                    fs.writeFileSync(jsonFileName, JSON.stringify(json));
+                },
+            }),
+        ]),
         commonjs(),  // 支持 commonjs 模块规范
         terser()  // 代码压缩
     ],
