0ba342c8aa4a0f76890330c1b932f7db8e7406e3
jiajing <jiajing@zhiweidata.com>
2021-09-15 10:10:37 +0800

    生产环境下打包配置更新

--- rollup.prod.js
+++ rollup.prod.js
@@ -5,10 +5,35 @@ import autoprefixer from "autoprefixer"
 import { terser } from "rollup-plugin-terser"
 
 export default {
-    input:"./src/index.js",
-    output:[
+    input: "./src/index.js",
+    output: [
         {
-            file:"./dist/ansheng-lib-dis"
+            file: "./dist/ansheng-lib-umd.js",
+            format: "umd",
+            name: "anshengLib"
+        }, {
+            file: "./dist/ansheng-lib-es.js",
+            format: "es"
+        }, {
+            file: "./dist/ansheng-lib-cjs.js",
+            format: "cjs"
         }
+    ],
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'  // 转换 es6 语法 
+        }),
+        vue({   // 编译 vue 代码，并为 vue 组件的样式加前缀
+            style: {
+                postcssPlugins: [
+                    autoprefixer()
+                ]
+            }
+        }),
+        commonjs(),  // 支持 commonjs 模块规范
+        terser()  // 代码压缩
+    ],
+    external: [
+        'vue'
     ]
 }
